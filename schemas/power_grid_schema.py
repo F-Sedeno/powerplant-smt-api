05ea3c853ee15d3f66a46360b48059cc4d5997fb
Hielito <fsedenoguerrero@gmail.com>
2025-11-18 12:42:08 +0100

    fix: add validation windows for fields

--- schemas/power_grid_schema.py
+++ schemas/power_grid_schema.py
@@ -5,13 +5,13 @@ from schemas.power_plant_schema import PowerPlantSchema
 
 class FuelSchema(BaseModel):
     model_config = ConfigDict(from_attributes=True)
-    gasfired: float = Field(validation_alias="gas(euro/MWh)")
-    turbojet: float = Field(validation_alias="kerosine(euro/MWh)")
-    co2: float = Field(validation_alias="co2(euro/ton)")
-    windturbine: float = Field(validation_alias="wind(%)")
+    gasfired: float = Field(validation_alias="gas(euro/MWh)", gt=0)
+    turbojet: float = Field(validation_alias="kerosine(euro/MWh)", gt=0)
+    co2: float = Field(validation_alias="co2(euro/ton)", gt=0)
+    windturbine: float = Field(validation_alias="wind(%)", ge=0, le=100)
 
 class PowerGridSchema(BaseModel):
     model_config = ConfigDict(from_attributes=True, json_schema_extra=POST_PRODUCTIONPLAN_EXAMPLE)
-    load: float
+    load: float = Field(gt=0)
     fuels: FuelSchema
     powerplants: list[PowerPlantSchema]
--- schemas/power_plant_schema.py
+++ schemas/power_plant_schema.py
@@ -1,14 +1,14 @@
 from typing_extensions import Literal
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 
 class PowerPlantSchema(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
     name: str
     type: Literal["gasfired", "turbojet", "windturbine"]
-    efficiency: float
-    pmax: int
-    pmin: int
+    efficiency: float = Field(gt=0)
+    pmax: int = Field(gt=0)
+    pmin: int = Field(ge=0)
     
 
 class PowerPlantResponseSchema(BaseModel):
